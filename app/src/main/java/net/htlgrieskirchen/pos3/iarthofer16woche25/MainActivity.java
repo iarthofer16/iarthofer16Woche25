c9c80292933578ce53b345109c89c5389f4f6e03
iarthofer16 <isabelle.arthofer@gmail.com>
2019-04-04 10:16:11 +0200

    Initial commit

---
+++ app/src/main/java/net/htlgrieskirchen/pos3/iarthofer16woche25/MainActivity.java
@@ -0,0 +1,13 @@
+package net.htlgrieskirchen.pos3.iarthofer16woche25;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
